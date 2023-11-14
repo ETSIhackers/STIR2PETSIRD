9f966c4bf545da4b375df66f405f6e0064675d7d
Robert-PrescientImaging <117300855+Robert-PrescientImaging@users.noreply.github.com>
2023-11-14 23:25:19 +0000

    Update build script and README

--- cpp/build_STIR_PETSIRD_convertor.sh
+++ cpp/build_STIR_PETSIRD_convertor.sh
@@ -1,16 +1,18 @@
 #! /bin/bash
 # FILEPATH: /workspaces/STIR2PETSIRD/cpp/build.sh
 
-# Utility script to build STIR_PETSIRD_convertor in the cpp dir
-STIR_DIR="/workspaces/STIR2PETSIRD/stir/install/lib/cmake/"
-PETSIRD_DIR="/workspaces/STIR2PETSIRD/cpp"
-cpp_dir="/workspaces/STIR2PETSIRD/cpp"
+# Utility script to help build STIR_PETSIRD_convertor in the cpp dir
+STIR2PETSIRD_DIR="/workspaces/STIR2PETSIRD"
+STIR_DIR="${STIR2PETSIRD_DIR}/stir/install/lib/cmake/"
+PETSIRD_DIR="${STIR2PETSIRD_DIR}/PETSIRD/cpp/generated"
+cpp_dir="${STIR2PETSIRD_DIR}/cpp"
+BUILD_DIR="${cpp_dir}/build"
 
-# rm -rf ${cpp_dir}/build
-if [ ! -d "${cpp_dir}/build" ]; then
-    mkdir -p "${cpp_dir}/build"
+# rm -rf ${BUILD_DIR}  # Optional
+if [ ! -d "${BUILD_DIR}" ]; then
+    mkdir -p "${BUILD_DIR}"
 fi
 
-cd ${cpp_dir}/build
+cd ${BUILD_DIR}
 cmake -G Ninja -S ${cpp_dir} -DHDF5_ROOT=${CONDA_PREFIX} -DSTIR_DIR=${STIR_DIR} -DCMAKE_PREFIX_PATH=${PETSIRD_DIR}
 ninja
