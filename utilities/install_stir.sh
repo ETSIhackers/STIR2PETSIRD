5a41c153914cee5a43402e10d6eb0759588300c8
Robert-PrescientImaging <117300855+Robert-PrescientImaging@users.noreply.github.com>
2023-11-14 20:01:20 +0000

    Install stir support - work around for conda issue

---
+++ utilities/install_stir.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# FILEPATH: /workspaces/STIR2PETSIRD/stir/install_stir.sh
+
+conda install boost
+
+STIR_DIR=/workspaces/STIR2PETSIRD/stir
+
+cd ${STIR_DIR}
+git clone https://github.com/UCL/STIR.git
+mkdir -p build install
+cd build
+
+CMAKE_OPTIONS="-DCMAKE_INSTALL_PREFIX=${STIR_DIR}/install -DBUILD_SWIG_PYTHON=OFF"
+cmake ${STIR_DIR}/STIR/ ${CMAKE_OPTIONS}
+ninja install
