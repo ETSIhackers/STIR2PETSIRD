b72ed6caeaf0003b8ef8bcdbe75d4a422e0c282e
Robert-PrescientImaging <117300855+Robert-PrescientImaging@users.noreply.github.com>
2023-11-14 21:00:56 +0000

    Fix issue with build/ not a dir

--- cpp/build_STIR_PETSIRD_convertor.sh
+++ cpp/build_STIR_PETSIRD_convertor.sh
@@ -6,7 +6,11 @@ STIR_DIR="/workspaces/STIR2PETSIRD/stir/install"
 PETSIRD_DIR="/workspaces/STIR2PETSIRD/cpp"
 cpp_dir="/workspaces/STIR2PETSIRD/cpp"
 
-# rm -rf ${cpp_dir}/build && mkdir -p ${cpp_dir}/build
+# rm -rf ${cpp_dir}/build
+if [ ! -d "${cpp_dir}/build" ]; then
+    mkdir -p "${cpp_dir}/build"
+fi
+
 cd ${cpp_dir}/build
-cmake -G Ninja -S .. -DHDF5_ROOT=${CONDA_PREFIX} -DSTIR_DIR=${STIR_DIR} -DCMAKE_PREFIX_PATH=${PETSIRD_DIR}
+cmake -G Ninja -S ${cpp_dir} -DHDF5_ROOT=${CONDA_PREFIX} -DSTIR_DIR=${STIR_DIR} -DCMAKE_PREFIX_PATH=${PETSIRD_DIR}
 ninja
