36ea625fae4389edc0326de82b95e52c5827e78f
Robert-PrescientImaging <117300855+Robert-PrescientImaging@users.noreply.github.com>
2023-11-14 00:32:32 +0000

    Initial testing of stir import and data

--- python/start.py
+++ python/start.py
@@ -1,5 +1,14 @@
 import sys
-# Currently hard-wire location of generated files. This will need to change!
-sys.path.append('../PETSIRD/python/')
 import prd
+import stir
 
+# Currently hard-wire location of generated files. This will need to change!
+STIR2PETSIRD_PATH = "/workspaces/STIR2PETSIRD"
+
+sys.path.append(f"{STIR2PETSIRD_PATH}/PETSIRD/python/")
+
+data_path = f"{STIR2PETSIRD_PATH}/data/root_header_test1.hroot"
+
+lm_data = stir.ListModeData.read_from_file(data_path)
+
+print("Done")
