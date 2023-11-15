63bfea9a3f4950e4366cf3023b904cd30e9e5f48
Robert-PrescientImaging <117300855+Robert-PrescientImaging@users.noreply.github.com>
2023-11-15 00:08:30 +0000

    Basic analysis script, rewrite of prd_analysis.py

---
+++ python/analysis.py
@@ -0,0 +1,42 @@
+import sys
+import numpy
+
+sys.path.insert(0, "/workspaces/STIR2PETSIRD/PETSIRD/python")
+import prd
+
+
+def analyse_data(filename: str):
+    with prd.BinaryPrdExperimentReader(filename) as reader:
+        header = reader.read_header()
+        print(f"Subject ID: {header.exam.subject.id}")
+        print(f"Number of detectors: {header.scanner.number_of_detectors()}")
+        print(f"Number of TOF bins: {header.scanner.number_of_tof_bins()}")
+        print(f"Number of energy bins: {header.scanner.number_of_energy_bins()}")
+
+        energy_bin_edges = header.scanner.energy_bin_edges
+        print(f"Energy bin edges: {energy_bin_edges}")
+        energy_mid_points = (energy_bin_edges[:-1] + energy_bin_edges[1:]) / 2
+        print(f"Energy mid points: {energy_mid_points}")
+
+        energy_1, energy_2 = 0.0, 0.0
+        num_events = 0
+        last_time = 0
+        for time_block in reader.read_time_blocks():
+            last_time = time_block.id * header.scanner.listmode_time_block_duration
+            for event in time_block.prompt_events:
+                energy_1 += energy_mid_points[event.energy_1_idx]
+                energy_2 += energy_mid_points[event.energy_2_idx]
+                num_events += 1
+
+        print(f"Last time block at {last_time} ms")
+        print(f"Number of events: {num_events}")
+        print(f"Average energy_1: {energy_1 / num_events}")
+        print(f"Average energy_2: {energy_2 / num_events}")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python analysis.py <input_file>")
+        sys.exit(1)
+
+    analyse_data(sys.argv[1])
